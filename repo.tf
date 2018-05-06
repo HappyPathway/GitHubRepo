57fc5cecb903b8a537e67966ee461cb0f1b9db61
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-06 10:06:42 -0700

    first commit

---
+++ repo.tf
@@ -0,0 +1,11 @@
+resource "github_repository" "repo" {
+  name        = "${var.repo_name}"
+  description = "${var.desciption}"
+  homepage_url = "${var.homepage}"
+  private = "${var.private}"
+  gitignore_template = "${var.gitignore_template}"
+  license_template = "${var.license_template}"
+  has_wiki = "${var.has_wiki}"
+  has_issues = "${var.has_issues}"
+  default_branch = "${var.default_branch}"
+}
---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "repo_name" {}
+variable "desciption" {}
+variable "homepage" {}
+variable "private" {
+    default = true
+}
+
+variable "gitignore_template" {
+    default = "Python"
+}
+
+variable "license_template" {
+    default = "apache-2.0"
+}
+
+variable "has_wiki" {
+    default = true
+}
+
+variable "has_issues" {
+    default = true
+}
+
+variable "default_branch" {
+    default = "master"
+}
